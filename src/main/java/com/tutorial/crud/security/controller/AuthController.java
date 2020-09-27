884e122bc2eba37690080b3f375a35e907df3ee6
oscarmurillo344 <54074512+oscarmurillo344@users.noreply.github.com>
2020-09-27 18:28:38 -0500

    version 1

--- src/main/java/com/tutorial/crud/security/controller/AuthController.java
+++ src/main/java/com/tutorial/crud/security/controller/AuthController.java
@@ -57,10 +57,6 @@ public class AuthController {
 
     @PostMapping("/nuevo")
     public ResponseEntity<?> nuevo(@Valid @RequestBody NuevoUsuario nuevoUsuario, BindingResult bindingResult){
-        Rol rolAdmin = new Rol(RolNombre.ROLE_ADMIN);
-        Rol rolUser = new Rol(RolNombre.ROLE_USER);
-            rolService.save(rolAdmin);
-            rolService.save(rolUser);
         if(bindingResult.hasErrors())
             return new ResponseEntity(new Mensaje("campos mal puestos o email inválido"), HttpStatus.BAD_REQUEST);
         if(usuarioService.existsByNombreUsuario(nuevoUsuario.getNombreUsuario()))
