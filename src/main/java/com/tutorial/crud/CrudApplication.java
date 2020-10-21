9e3e195a84235ce7ffe50559f186809bf556ae11
oscarmurillo344 <54074512+oscarmurillo344@users.noreply.github.com>
2020-10-21 13:57:31 -0500

    version 1

--- src/main/java/com/tutorial/crud/CrudApplication.java
+++ src/main/java/com/tutorial/crud/CrudApplication.java
@@ -1,14 +1,20 @@
 package com.tutorial.crud;
 
+import com.tutorial.crud.security.service.RolService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CrudApplication {
 
+	@Autowired
+	RolService rolservice;
+
 	public static void main(String[] args) {
 
 		SpringApplication.run(CrudApplication.class, args);
+
 	}
 
 }
