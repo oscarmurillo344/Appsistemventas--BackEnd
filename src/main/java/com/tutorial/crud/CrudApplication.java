a5f36dd8d94d3d80f1e58763416459710ca65a76
oscarmurillo344 <54074512+oscarmurillo344@users.noreply.github.com>
2020-10-21 14:08:13 -0500

    version 1

--- src/main/java/com/tutorial/crud/CrudApplication.java
+++ src/main/java/com/tutorial/crud/CrudApplication.java
@@ -1,20 +1,13 @@
 package com.tutorial.crud;
 
-import com.tutorial.crud.security.service.RolService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CrudApplication {
 
-	@Autowired
-	RolService rolservice;
-
 	public static void main(String[] args) {
-
 		SpringApplication.run(CrudApplication.class, args);
-
 	}
 
 }
--- src/main/java/com/tutorial/crud/security/controller/AuthController.java
+++ src/main/java/com/tutorial/crud/security/controller/AuthController.java
@@ -65,13 +65,10 @@ public class AuthController {
             return new ResponseEntity(new Mensaje("ese nombre ya existe"), HttpStatus.BAD_REQUEST);
         if(usuarioService.existsByEmail(nuevoUsuario.getEmail()))
             return new ResponseEntity(new Mensaje("ese email ya existe"), HttpStatus.BAD_REQUEST);
-
-       if(rolService.getByRolNombre(RolNombre.ROLE_ADMIN).get() == null){
-           Rol rolAdmin = new Rol(RolNombre.ROLE_ADMIN);
-           Rol rolUser = new Rol(RolNombre.ROLE_USER);
-           rolService.save(rolAdmin);
-           rolService.save(rolUser);
-       }
+        Rol rolAdmin = new Rol(RolNombre.ROLE_ADMIN);
+        Rol rolUser = new Rol(RolNombre.ROLE_USER);
+        rolService.save(rolAdmin);
+        rolService.save(rolUser);
         Usuario usuario =
                 new Usuario(nuevoUsuario.getNombre(), nuevoUsuario.getNombreUsuario(), nuevoUsuario.getEmail(),
                         passwordEncoder.encode(nuevoUsuario.getPassword()));
