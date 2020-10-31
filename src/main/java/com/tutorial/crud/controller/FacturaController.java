a2d5f7c9b9e1c6d2def9cd02331298eb463619d1
oscarmurillo344 <54074512+oscarmurillo344@users.noreply.github.com>
2020-10-31 13:20:11 -0500

    version 1

--- src/main/java/com/tutorial/crud/controller/FacturaController.java
+++ src/main/java/com/tutorial/crud/controller/FacturaController.java
@@ -14,9 +14,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Calendar;
+
 import java.util.List;
-import java.util.TimeZone;
 
 @RestController
 @RequestMapping("/factura")
